edb22325dbb76795f72c2587bfcd281a89a150cc
jlmunozpb <jlmunozpb@gmail.com>
2023-11-28 22:50:21 -0500

    main

--- src/home.js
+++ src/home.js
@@ -13,74 +13,72 @@ export const Inicio = () => {
   return (
     <div className="App">
       <center>
-        <table>
-            <thead>
-              <tr>
-                <td><h3> Consultorio Odontológico </h3></td>
-              </tr>
-            </thead>
-            <tbody>
-              <center>
-                <tr>
-                  <td className='App-body-icono'>
-                    <TbDental size={iconSize}/>
-                    <p>Blanqueamiento</p>
-                  </td>
-                  <td className='App-body-icono'>
-                    <TbDentalBroken size={iconSize}/>
-                    <p>Endodoncia</p>
-                  </td>
-                  <td className='App-body-icono'>
-                    <TbDentalOff size={iconSize}/>
-                    <p>Cirugía</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td className='App-body-icono'>
-                    <LiaToothSolid size={iconSize}/>
-                    <p>Odontopediatría</p>
-                  </td>
-                  <td className='App-body-icono'>
-                    <PiTooth size={iconSize}/>
-                    <p>Ortodoncia</p>
-                  </td>
-                  <td className='App-body-icono'>
-                    <LiaTeethSolid size={iconSize}/>
-                    <p>Periodoncia</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td className='App-body-icono'>
-                    <FaTeeth size={iconSize}/>
-                    <p>Radiología</p>
-                  </td>
-                  <td className='App-body-icono'>
-                    <GiTooth size={iconSize}/>
-                    <p>Prostodoncia</p>
-                  </td>
-                  <td className='App-body-icono'>
-                    <FaTooth size={iconSize}/>
-                    <p>Odontología</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td className='App-body-icono'>
-                    <LiaTeethOpenSolid size={iconSize}/>
-                    <p>Diseño de Sonrisa</p>
-                  </td>
-                  <td className='App-body-icono'>
-                    <PiToothFill size={iconSize}/>
-                    <p>Patología</p>
-                  </td>
-                  <td className='App-body-icono'>
-                    <FaTeethOpen size={iconSize}/>
-                    <p>Profilaxis</p>
-                  </td>
-                </tr>
-              </center>
-            </tbody>
-          </table>
-        </center>
+        <table className='contentHome'>
+          <thead>
+            <tr>
+              <th colSpan={3}><h3> Consultorio Odontológico </h3></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className='App-body-icono'>
+                <TbDental size={iconSize}/>
+                <p>Blanqueamiento</p>
+              </td>
+              <td className='App-body-icono'>
+                <TbDentalBroken size={iconSize}/>
+                <p>Endodoncia</p>
+              </td>
+              <td className='App-body-icono'>
+                <TbDentalOff size={iconSize}/>
+                <p>Cirugía</p>
+              </td>
+            </tr>
+            <tr>
+              <td className='App-body-icono'>
+                <LiaToothSolid size={iconSize}/>
+                <p>Odontopediatría</p>
+              </td>
+              <td className='App-body-icono'>
+                <PiTooth size={iconSize}/>
+                <p>Ortodoncia</p>
+              </td>
+              <td className='App-body-icono'>
+                <LiaTeethSolid size={iconSize}/>
+                <p>Periodoncia</p>
+              </td>
+            </tr>
+            <tr>
+              <td className='App-body-icono'>
+                <FaTeeth size={iconSize}/>
+                <p>Radiología</p>
+              </td>
+              <td className='App-body-icono'>
+                <GiTooth size={iconSize}/>
+                <p>Prostodoncia</p>
+              </td>
+              <td className='App-body-icono'>
+                <FaTooth size={iconSize}/>
+                <p>Odontología</p>
+              </td>
+            </tr>
+            <tr>
+              <td className='App-body-icono'>
+                <LiaTeethOpenSolid size={iconSize}/>
+                <p>Diseño Sonrisa</p>
+              </td>
+              <td className='App-body-icono'>
+                <PiToothFill size={iconSize}/>
+                <p>Patología</p>
+              </td>
+              <td className='App-body-icono'>
+                <FaTeethOpen size={iconSize}/>
+                <p>Profilaxis</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </center>
     </div>
   )
 }
