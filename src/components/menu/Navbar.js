d2b8cb884320a5c04b8db5e28e0246ab90219919
jlmunozpb <jlmunozpb@gmail.com>
2024-01-04 16:10:45 -0500

    main

--- src/components/menu/Navbar.js
+++ src/components/menu/Navbar.js
@@ -19,13 +19,13 @@ export const Navbar = ({urlBaseFrontend, myColor, myTitle}) => {
     return (
         <nav className="navbar navbar-expand-sm navbar-light bg-white fixed-top shadow-lg user-select-none">
             <div className="container-fluid">
-                <Logo color={myColor} className='ms-3'/>
+                <Logo color={myColor} height={1.25} width={1.25} className='ms-3'/>
                 <Link className="navbar-brand main-color fs-sm-2 ps-2" to={"/" + urlBaseFrontend + "/index"} >{ myTitle }</Link>
                 <div className="navbar-collapse d-none"  id="navbarContent">
                     <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                     </div>
                 </div>
-                <button className="btn btn-light main-color" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBody" aria-controls="offcanvasBody"><Home color={myColor} height={1.1} width={1.1}/></button>
+                <button className="btn btn-light main-color" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBody" aria-controls="offcanvasBody"><Home color={myColor} height={1.25} width={1.25}/></button>
             </div>
         </nav>
     )
