91b2d3598940242e22b36c613e6ab0fd747f1ca2
jlmunozpb <jlmunozpb@gmail.com>
2024-10-28 08:00:26 -0500

    main

--- src/classes/Alert.js
+++ src/classes/Alert.js
@@ -1,7 +1,8 @@
 
 export class Alert {
-    constructor({ title:title='', message:message='' }) {
+    constructor({ title:title='', message:message='', buttons:buttons='' }) {
         this.title = {title}.title;
         this.message = {message}.message;
+        this.buttons = {buttons}.buttons;
     }
 }
