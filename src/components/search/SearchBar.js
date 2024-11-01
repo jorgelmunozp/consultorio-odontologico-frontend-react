c42ad93ca28bcd0ea1ce89ad9cdd8b4cef87d0ae
jlmunozpb <jlmunozpb@gmail.com>
2024-11-01 10:02:55 -0500

    main

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -12,13 +12,11 @@ export const SearchBar = ({ icon='🔎',items,queries,setQueries,className }) =>
                 <div className="card card-body shadow-sm">
                     <div className='container-fluid'>
                         <div className='row d-block d-sm-flex'>
-                            {
-                                items.map((item, index)=>{
-                                    console.log("item SearchBar", item)
-                                    return (
-                                        // <Input property={{key:item.title, value:queries[index], type:item.type, handleChange:(target) => setQueries[index](target.target.value) }} className={'col input form-control rounded border-muted border-1 text-muted text-center shadow-sm'} />
-                                        <Input key={item.type+index} value={queries[index]} type={item.type} onChange={(target) => setQueries[index](target.target.value) } placeholder={item.title.charAt(0).toUpperCase() + item.title.slice(1)} className={'input form-control rounded border-muted border-1 text-muted text-center shadow-sm'} />
-                                    )
+                            <Input placeholder={'Código'} key={'number0'} value={queries[0]} type={'number'} onChange={(target) => setQueries[0](target.target.value) } className={'input form-control rounded border-muted border-1 text-muted text-center shadow-sm'} />
+                        </div>
+                        <div className='row d-block d-sm-flex'>
+                            {   items.map((item, index)=>{
+                                    return ( <Input key={item.type+index} value={queries[index+1]} type={item.type} onChange={(target) => setQueries[index+1](target.target.value) } placeholder={item.title.charAt(0).toUpperCase() + item.title.slice(1)} className={'input form-control rounded border-muted border-1 text-muted text-center shadow-sm'} /> )
                                 })
                             }
                         </div>
