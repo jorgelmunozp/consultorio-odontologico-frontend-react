8766b365d5c9fada740912bb5a04ef163391e863
jlmunozpb <jlmunozpb@gmail.com>
2025-08-07 22:53:39 -0500

    main

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { createRoot } from 'react-dom/client';
 import { Suspense, lazy } from 'react';
 import { Logo } from '../src/components/icons/logo/Logo.js';
-import { myColor } from './global';
+import { myColor } from './global.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'animate.css';
 
-const App = lazy(() => import('./App'));
+const preloadApp = import('./App');
+const App = lazy(() => preloadApp);
+// const App = lazy(() => import('./App'));
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <Suspense delayMs={0} fallback={<div style={{display:'flex', height:'100vh', width:'100%', alignItems:'center', justifyContent:'center', animation:'splash 0.75s linear infinite' }}><Logo height={10} width={10} strokeWidth={1} color={myColor} /></div>}>
+  <Suspense fallback={<div style={{display:'flex', height:'100vh', width:'100%', alignItems:'center', justifyContent:'center', animation:'splash 0.75s linear infinite' }}><Logo height={10} width={10} strokeWidth={1} color={myColor} /></div>}>
     <App Logo={Logo} />
   </Suspense>
 );
